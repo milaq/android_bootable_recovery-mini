dd3e09de26227b872e181ede00a3d887bf0b943b
Tom Marshall <tdm@cyngn.com>
2014-12-01 08:59:13 -0800

    recovery: Fix adb backup/restore
    Communication with adb is now done via stdin/stdout.  Dupe these
    descriptors and use them for read/write, and redirect the actual
    stdin/stdout to the recovery log file.
    
    Also limit progress updates to once per second to avoid overrun
    on the messages socket, and silence some debug output.
    
    Change-Id: I674fc9c38d94ea411f4ec5b271ed7a977c31aa93
    
    Conflicts:
    
    	restore.cpp
    	ui.cpp

--- bu.h
+++ bu.h
@@ -28,7 +28,8 @@ typedef struct md5 MD5_CTX;
 
 #define PROP_LINE_LEN (PROPERTY_KEY_MAX+1+PROPERTY_VALUE_MAX+1+1)
 
-extern int sockfd;
+extern int adb_ifd;
+extern int adb_ofd;
 extern TAR* tar;
 extern gzFile gzf;
 
@@ -56,7 +57,7 @@ extern void part_set(partspec* part);
 
 extern int update_progress(uint64_t off);
 
-extern int create_tar(const char* compress, const char* mode);
+extern int create_tar(int fd, const char* compress, const char* mode);
 
 extern int do_backup(int argc, char** argv);
 extern int do_restore(int argc, char** argv);
